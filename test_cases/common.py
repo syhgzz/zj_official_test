ddaa3d855c10e5c54e64f6916a4e3fd62c0f1e18
Wang_Sy <wangshiyu@zju.edu.cn>
2026-08-20 17:56:55 +0800

    增加株洲坐标值

--- test_cases/common.py
+++ test_cases/common.py
@@ -5,11 +5,13 @@ loc_list = {
     '北京':[115.814, 116.816, 39.637, 40.423], # 北京
     '唐山':[117.903, 118.825, 39.185, 40.131], # 唐山
     '天津':[116.919, 117.831, 38.695, 39.525], # 天津
+    '株洲':[112.975, 114.115, 26.038, 28.021], # 株洲
     '全国':[83.353, 126.196, 21.928, 47.096], # 全国
 }
 #根据测试需求切换默认值
 
-minLng_global, maxLng_global, minLat_global, maxLat_global = loc_list['重庆']
+# minLng_global, maxLng_global, minLat_global, maxLat_global = loc_list['重庆']
+minLng_global, maxLng_global, minLat_global, maxLat_global = loc_list['株洲']
 startTime_global = int(datetime(2026,5,5,0,0,0).timestamp()) * 1000    
 endTime_global = int(datetime(2026,6,5,23,59,59).timestamp()) * 1000
 
