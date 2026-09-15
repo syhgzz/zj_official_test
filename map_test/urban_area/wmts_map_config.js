a8face4adb9b4285229caffbefafee8e047b9662
Jing Yao <1429404521@qq.com>
2026-09-15 16:19:45 +0800

    城市测试程序精简

---
+++ map_test/urban_area/wmts_map_config.js
@@ -0,0 +1,9 @@
+window.WMTS_MAP_CONFIG = Object.freeze({
+  amap: Object.freeze({
+    key: '244262d7f08882349099fad8cd2ae0cc',
+    securityJsCode: 'c950d2e523f6087f865698235d3c87d5'
+  }),
+  tianditu: Object.freeze({
+    token: 'f9e2e3441d7d86de48ff3a542c1c890c'
+  })
+})
